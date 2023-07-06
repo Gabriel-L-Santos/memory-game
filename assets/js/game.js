2aad0f944f794b73d843bd426e94ea91e41b7857
Gabriel Leme dos Santos <gabriellemesantos@gmail.com>
2023-07-06 19:57:18 -0300

    add tempo

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,6 @@
 const grid = document.querySelector('.grid');
 const spanPlayer = document.querySelector('.player');
+const timer = document.querySelector('.timer');
 
 const characters = [
     'biff-tannen',
@@ -111,12 +112,16 @@ const loadGame = () => {
     });
 }
 
+const startTimer = () => {
+    setInterval(() => {
+        const currentTime = +timer.innerHTML;
+        timer.innerHTML = currentTime + 1;
+    }, 1000);
+}
+
 window.onload = () => {
-
-    const playerName = localStorage.getItem('player');
-
-    spanPlayer.innerHTML = playerName;
-
+    spanPlayer.innerHTML = localStorage.getItem('player');;
+    startTimer();
     loadGame(); 
 }
 
